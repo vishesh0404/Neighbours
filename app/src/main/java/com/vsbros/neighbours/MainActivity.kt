45e234a188fe4a70448d728d5d36482efabefd50
vishesh <vishesh0404@gmail.com>
2020-04-09 23:18:47 +0530

    Section 2, Commit 2

--- app/src/main/java/com/vsbros/neighbours/MainActivity.kt
+++ app/src/main/java/com/vsbros/neighbours/MainActivity.kt
@@ -2,11 +2,21 @@ package com.vsbros.neighbours
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_information.setOnClickListener {
+
+            Log.i("MainActivity", "Clicked on Add Image")
+
+            main_text.setText("Text Gets Changed")
+        }
+
     }
 }
